68332053fb07e0f5eb93e68f9cffe264b260f384
Ariel Behar <ariel.behar@hotmail.com>
2022-11-09 19:49:34 +0200

    Modifying the appendChild style of creating an element to innerHtml

--- homework2/main.js
+++ homework2/main.js
@@ -18,10 +18,14 @@ searchForm.addEventListener('submit', (e) =>{
     }
 })
 
-function displaySearchResults(results) {
+function clearSearchResultsSection() {
     while(searchResultsSection.firstChild){
         searchResultsSection.removeChild(searchResultsSection.lastChild)
     }
+}
+
+function displaySearchResults(results) {
+    clearSearchResultsSection()
     
     for (const book of results.items) {
         createBook(book)
@@ -36,63 +40,75 @@ function displaySearchResults(results) {
 
 }
 
-
 function createBook(book) {
     let bookCardArticle = document.createElement('article');
     bookCardArticle.classList.add('book-card')
+    bookCardArticle.innerHTML = `<header class="book-heading">
+            <h3 class="book-title">${book.volumeInfo.title}</h3>
+            <h4 class="book-author">by ${book.volumeInfo.authors ? `by ${book.volumeInfo.authors.join(' & ')}` : 'author uknown'}</h4>
+        </header><img class="book-image" src="${book.volumeInfo.imageLinks.thumbnail}">
+        <p class="book-description">${generateDescription()}</p>
+        <div class="favorites-div">
+            <p>Add to Favorites<span class="material-symbols-outlined">star</span></p>
+        </div>`;
 
-    let bookHeader = document.createElement('header')
-    bookHeader.classList.add('book-heading')
 
-    let bookTitle = document.createElement('h3')
-    bookTitle.classList.add('book-title')
-    bookTitle.textContent = book.volumeInfo.title
-
-    let bookAuthor = document.createElement('h4')
-    bookAuthor.classList.add('book-author')
-    bookAuthor.textContent = book.volumeInfo.authors ? `by ${book.volumeInfo.authors.join(' & ')}` : 'author uknown'
-
-    let bookImage = document.createElement('img')
-    bookImage.classList.add('book-image')
-    bookImage.src = book.volumeInfo.imageLinks.thumbnail
-
-    let bookDescription = document.createElement('p')
-    bookDescription.classList.add('book-description')
-    if(book.volumeInfo.description) {
-        if(book.volumeInfo.description.length >= 300) {
-            let truncatedDescription = book.volumeInfo.description.slice(0,300)
-
-            truncatedDescription = truncatedDescription.split(' ')
-            truncatedDescription.push('...')
-            console.log(truncatedDescription.join(' '))
-            bookDescription.textContent = truncatedDescription.join(' ')
-            console.log('places')
-        } else {
-            bookDescription.textContent = book.volumeInfo.description
-            console.log('notep');
+    function generateDescription(){
+        if(book.volumeInfo.description) {
+            if(book.volumeInfo.description.length >= 300) {
+                let truncatedDescription = book.volumeInfo.description.slice(0,300)
+    
+                truncatedDescription = truncatedDescription.split(' ')
+                truncatedDescription.push('...')
+                return truncatedDescription.join(' ')
+            } else {
+                return book.volumeInfo.description
+            }
         }
     }
 
-    let favoritesDiv = document.createElement('div')
-    favoritesDiv.classList.add('favorites-div')
-
-    let favoritesParagraph = document.createElement('p')
-    favoritesParagraph.textContent = 'Add to Favorites'
-
-    let starSpan = document.createElement('span')
-    starSpan.textContent = 'star'
-    starSpan.classList.add('material-symbols-outlined')
-
-    favoritesParagraph.appendChild(starSpan)
-    favoritesDiv.appendChild(favoritesParagraph)
-
-    bookHeader.appendChild(bookTitle)
-    bookHeader.appendChild(bookAuthor)
-    bookCardArticle.appendChild(bookHeader)
-    bookCardArticle.appendChild(bookImage)
-    bookCardArticle.appendChild(bookDescription)
-    bookCardArticle.appendChild(favoritesDiv)
 
     searchResultsSection.appendChild(bookCardArticle)
 }
 
+
+
+    
+    // let bookHeader = document.createElement('header')
+    // bookHeader.classList.add('book-heading')
+
+    // let bookTitle = document.createElement('h3')
+    // bookTitle.classList.add('book-title')
+    // bookTitle.textContent = book.volumeInfo.title
+
+    // let bookAuthor = document.createElement('h4')
+    // bookAuthor.classList.add('book-author')
+    // bookAuthor.textContent = book.volumeInfo.authors ? `by ${book.volumeInfo.authors.join(' & ')}` : 'author uknown'
+
+    // let bookImage = document.createElement('img')
+    // bookImage.classList.add('book-image')
+    // bookImage.src = book.volumeInfo.imageLinks.thumbnail
+
+    // let bookDescription = document.createElement('p')
+    // bookDescription.classList.add('book-description')
+
+    // let favoritesDiv = document.createElement('div')
+    // favoritesDiv.classList.add('favorites-div')
+    // favoritesDiv.innerHTML =``
+
+    // let favoritesParagraph = document.createElement('p')
+    // favoritesParagraph.textContent = 'Add to Favorites'
+
+    // let starSpan = document.createElement('span')
+    // starSpan.textContent = 'star'
+    // starSpan.classList.add('material-symbols-outlined')
+
+    // favoritesParagraph.appendChild(starSpan)
+    // favoritesDiv.appendChild(favoritesParagraph)
+
+    // bookHeader.appendChild(bookTitle)
+    // bookHeader.appendChild(bookAuthor)
+    // bookCardArticle.appendChild(bookHeader)
+    // bookCardArticle.appendChild(bookImage)
+    // bookCardArticle.appendChild(bookDescription)
+    // bookCardArticle.appendChild(favoritesDiv)
